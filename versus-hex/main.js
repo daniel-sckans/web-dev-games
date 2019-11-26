65951b61bb459a6c269c0a374492a32017babeb7
Evan Daniel <Evan.Daniel@sckans.edu>
2019-11-26 04:43:08 -0600

    versus hex initial commit

--- versus-hex/main.js
+++ versus-hex/main.js
@@ -1,111 +1,112 @@
 const forceBackendUrl = false; 
 const hexCharacters = '0123456789abcdef'; 
+let socket; 
+let RGB = {R: '0', G: '0', B: '0'}; 
+let name = ''; 
+const messages = {}; 
+[
+	'INITIALIZE',
+	'JOIN',
+	'WAIT',
+	'START',
+	'PLAY',
+	'MOVE',
+	'RESULT',
+	'RESIGN',
+	'TIMEOUT',
+	'SUCCESS',
+    'ERROR',
+].forEach((s, i) => messages[s] = i); 
 
-window.addEventListener('DOMContentLoaded', dOMContentLoaded => {
-    socket_init(); 
-
-    document.querySelectorAll('enter-color > *').forEach((enterColorChild, rGBIndex) => {
-        for(let i = 0; i < 16; i++) {
-            const index = hexCharacters.charAt(i); 
-            const colorButton = document.createElement('div'); 
-            colorButton.style.backgroundColor = `#${['f00', '0f0', '00f'][rGBIndex].replace('f', hexCharacters.charAt(i))}`; 
-            colorButton.innerHTML = index; 
-            enterColorChild.append(colorButton); 
-        }; 
-    }); 
-    
-    let problemColor = newProblemColor(); 
-    document.querySelector('accuracy > span').innerText = colorAccuracyPercent(problemColor, '000'); 
-    
-    let clicksCounter = 0; 
-    let staticClicksCounter = 0; 
-    let winsCounter = 0; 
-
-    document.querySelector('enter-color').addEventListener('click', enterColorClick => {
-        if(document.querySelector('problem-color').className == 'success') return; 
-        const enterColorValue = enterColorClick.target.innerText; 
-        document.querySelector('preview-color-' + enterColorClick.target.parentElement.tagName).innerText = enterColorValue; 
-        
-        enterColorClick.target.parentElement.querySelectorAll('.checked').forEach(checked => checked.className = ''); 
-        enterColorClick.target.className = 'checked'; 
-        document.querySelector('clicks > span').innerText = ++clicksCounter; 
-        staticClicksCounter++; 
-        document.querySelector('average-clicks > span').innerText = (staticClicksCounter/winsCounter).toFixed(2); 
-        
-        let previewColorHexValue = ''; 
-        document.querySelectorAll('preview-color > *').forEach(previewColorChild => previewColorHexValue += previewColorChild.innerText ? previewColorChild.innerText : '0'); 
-        document.querySelector('preview-color').style.backgroundColor = `#${previewColorHexValue}`; 
-
-        const colorAccuracy = colorAccuracyPercent(problemColor, previewColorHexValue); 
-        document.querySelector('accuracy span').innerText = colorAccuracy; 
-
-        if(colorAccuracy === '100.00%') {
-            document.querySelectorAll('problem-color > *').forEach((problemColorChild, problemColorIndex) => problemColorChild.innerText = problemColor.charAt(problemColorIndex)); 
-            document.querySelector('problem-color').className = 'success'; 
-            document.querySelector('wins > span').innerText = ++winsCounter; 
-            document.querySelector('average-clicks > span').innerText = (staticClicksCounter/winsCounter).toFixed(2); 
-            const score = Math.floor(Math.pow(winsCounter, 2)/staticClicksCounter * 100); 
-            document.querySelector('wins-per-click > span').innerText = score; 
-            localStorage.setItem('guessTheHexWins', +localStorage.getItem('guessTheHexWins') + 1); 
-            if(!localStorage.getItem('guessTheHexHighScore') || +localStorage.getItem('guessTheHexHighScore') < +score) {
-                localStorage.setItem('guessTheHexHighScore', score); 
-            }
-            sendWin(); 
-        } 
-    }); 
-
-    document.querySelector('play-again').addEventListener('click', () => {
-        problemColor = newProblemColor(); 
-        document.querySelector('average-clicks > span').innerText = (staticClicksCounter/winsCounter).toFixed(2); 
-        document.querySelector('problem-color').className = ''; 
-        clicksCounter = 0; 
-        document.querySelector('clicks > span').innerText = clicksCounter; 
-        let previewColorHexValue = ''; 
-        document.querySelectorAll('preview-color > *').forEach(previewColorChild => previewColorHexValue += previewColorChild.innerText ? previewColorChild.innerText : '0'); 
-        document.querySelector('accuracy > span').innerText = colorAccuracyPercent(problemColor, previewColorHexValue); 
-        document.querySelectorAll('problem-color > *').forEach(problemColorChild => problemColorChild.innerText = '?'); 
-    }); 
+window.addEventListener('DOMContentLoaded', () => {
+    socket = new WebSocket(!forceBackendUrl && window.location.hostname === 'localhost' ? 'ws://localhost:8080/web_dev_games_versus_backend' : 'wss://www.el-evan.com/web_dev_games_versus_backend'); 
+    socket.addEventListener('error', displayError); 
+    socket.addEventListener('close', displayError); 
+    socket.addEventListener('open', main);
 }); 
 
-const colorAccuracyPercent = (c1, c2) => {
-    let diff = 0; 
-    for(let i = 0; i < c1.length; i++) {
-        diff += Math.abs(parseInt(c1.charAt(i), 16) - parseInt(c2.charAt(i), 16)); 
-    }
-    return ((1 - diff/48) * 100).toFixed(2) + '%'; 
-}; 
-
-const newProblemColor = () => {
-    let problemColor = ''; 
-    for(let i = 0; i < 3; i++) problemColor += hexCharacters.charAt(Math.floor(Math.random()*16)); 
-    document.querySelector('problem-color').style.backgroundColor = `#${problemColor}`; 
-    return problemColor; 
-}; 
-
-const sendWin = () => {
-    const message = {
-        Name: localStorage.getItem("name"), 
-        Game: "guess_the_hex"
-    }; 
-    if(!message.Name) return;
-    
-    const xhr = new XMLHttpRequest(); 
-    xhr.open('POST', !forceBackendUrl && window.location.hostname === 'localhost' ? 'http://localhost:8080/web-dev-games' : 'https://web-dev-games-mr44t3zfia-uc.a.run.app/web-dev-games'); 
-    xhr.setRequestHeader('Content-Type', 'application/json'); 
-    xhr.send(JSON.stringify(message)); 
-
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText); 
-            console.log('XHR response: ', response.Status); 
-        }
-    }; 
-}; 
-
-const socket_init = () => {
-    const socket = new WebSocket(!forceBackendUrl && window.location.hostname === 'localhost' ? 'ws://localhost:8080/versus_hex' : 'wss://future-app-location'); 
-
-    socket.addEventListener('open', () => {
-        console.log('I think socket works.'); 
+const main = () => {
+    document.querySelector('new-match').addEventListener('click', () => {
+        if(!getName()) return; 
+        socket.send(JSON.stringify({State: messages.JOIN, Payload: getName()}));
+        document.querySelector('select').setAttribute('disabled', 'disabled'); 
     }); 
+    document.querySelector('resign').addEventListener('click', () => socket.send(JSON.stringify({State: messages.RESIGN}))); 
+
+    document.querySelector('enter-color').addEventListener('click', colorClick => {
+        RGB[colorClick.target.parentElement.tagName] = colorClick.target.innerText; 
+        socket.send(JSON.stringify({State: messages.MOVE, Payload: getRGB()})); 
+    }); 
+    
+    socket.addEventListener('message', socketMessage => {
+        console.log('socket message: ' + socketMessage.data); 
+        
+        let yourMove; 
+        const message = JSON.parse(socketMessage.data); 
+        switch(message.State) {
+            case messages.WAIT: 
+                console.log('WAIT message'); 
+                setFeedback('You are currently waiting for an opponent.'); 
+                setEnterColorActive(false); 
+                break; 
+            case messages.START: 
+                console.log('START message'); 
+                setOpponent(message.Names[1-message.Names.indexOf(name)]); 
+                yourMove = message.Names.indexOf(name) === message.Moves.length % 2; 
+                setEnterColorActive(yourMove); 
+                setFeedback(yourMove ? '' : `Waiting for ${message.Names[1-message.Names.indexOf(name)]} to move.`); 
+                break; 
+            case messages.MOVE: 
+                console.log('MOVE message'); 
+                setRGB(message.Moves[message.Moves.length-1]); 
+                yourMove = message.Names.indexOf(name) === message.Moves.length % 2; 
+                setEnterColorActive(yourMove); 
+                setFeedback(yourMove ? '' : `Waiting for ${message.Names[1-message.Names.indexOf(name)]} to move.`); 
+                break; 
+            case messages.SUCCESS: 
+                console.log('SUCCESS message'); 
+                setEnterColorActive(false); 
+                setFeedback(`Someone Won!  It was ${message.Winner}.`); 
+                break; 
+            case messages.RESIGN: 
+                console.log('RESIGN message'); 
+                setFeedback(`Someone resigned.  The player who resigned was ${message.Names[1 - message.Names.indexOf(message.Winner)]}.  That means that ${message.Winner} won.  The game.`); 
+                setEnterColorActive(false); 
+                break; 
+            case messages.TIMEOUT: 
+                console.log('TIMEOUT message'); 
+                break; 
+            case messages.ERROR: 
+                console.log('ERROR message'); 
+                setEnterColorActive(false); 
+                setFeedback(`There was an error on the part of ${message.Names[1 - message.Names.indexOf(message.Winner)]}.  That means that ${message.Winner} won.  Wooo.`); 
+            default: 
+                console.log('ERROR message'); 
+                break
+        }
+    }); 
+}; 
+
+const getRGB = () => `${RGB.R}${RGB.G}${RGB.B}`; 
+const setRGB = (goal) => {
+    Object.keys(RGB).forEach((k, index) => RGB[k] = String.fromCharCode(goal[index])); 
+    document.querySelector('problem-color').style.backgroundColor = `#${getRGB()}`; 
+}; 
+
+const getName = () => {
+    const select = document.querySelector('select'); 
+    name = select[select.selectedIndex].innerText; 
+    if(name === 'select your name') name = ''; 
+    return name; 
+};
+
+const setEnterColorActive = isActivate => {
+    document.querySelector('blocker').style.display = isActivate ? 'none' : 'block'; 
+}; 
+
+const setFeedback = s => document.querySelector('feedback').innerText = s; 
+const setOpponent = s => document.querySelector('opponent > span').innerText = s; 
+
+const displayError = () => {
+    document.querySelector('error').style.display = 'flex'; 
 }; 
